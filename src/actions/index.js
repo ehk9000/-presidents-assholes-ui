41e0551c4f62d67967da9f949878b0962299892f
Erik Konner <erikkonner@gmail.com>
2019-06-03 09:34:02 -0600

    Wrote getAll, isLoading and SetError actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,14 @@
 export const getAllPresidents = (presidents) => ({
-  
-})
+  type: 'GET_ALL_PRESIDENTS',
+  presidents
+});
+
+export const isLoading = (bool) => ({
+  type: 'SET_IS_LOADING',
+  isLoading: bool
+});
+
+export const setError = (error) => ({
+  type: 'SET_ERROR',
+  error
+});
