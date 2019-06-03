c3394e2c744455a8d8ddf752979f5e13b5e14aba
Erik Konner <erikkonner@gmail.com>
2019-06-03 09:49:24 -0600

    Set up rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,13 @@
-import { comnbineReducers, combineReducers } from 'redux';
+import { combineReducers } from 'redux';
+import { presidentsReducer } from './presidentsReducer';
+import { isLoadingReducer } from './isLoadingReducer';
+import { setErrorReducer } from './setErrorReducer';
+
 
 const rootReducer = combineReducers({
-
+  presidents: presidentsReducer,
+  isLoading: isLoadingReducer,
+  error: setErrorReducer
 });
 
 export default rootReducer;
