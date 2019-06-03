6d11f6800a7af3fc9a0a53797645ae5667d4ea0d
Erik Konner <erikkonner@gmail.com>
2019-06-03 11:01:11 -0600

    pres details all displaying on dom

--- src/components/PresDetails/PresDetails.js
+++ src/components/PresDetails/PresDetails.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
 const PresDetails = (props) => {
-  console.log(props);
-  
+  let {
+    number, president, birth_year, death_year,
+     took_office,left_office, party} = props;
   return (
     <article>
-
+      <ul>
+        <li>Number: {number}</li>
+        <li>Name: {president}</li>
+        <li>Born: {birth_year}</li>
+        <li>Died: {death_year}</li>
+        <li>Took office: {took_office}</li>
+        <li>Left office: {left_office}</li>
+        <li>Party: {party}</li>
+      </ul>
     </article>
   )
 }
